b6309c3ff40fa3aefc9a9837e1b1945aa6fad71e
amnatest <ashraffatima101@gmail.com>
2026-01-05 22:12:24 -0600

    revert

--- vrr/vrr/Assets/Scripts/Isoline.cs
+++ vrr/vrr/Assets/Scripts/Isoline.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 //Makes sure any GameObject this script is attached to always has a MeshFilter and MeshRenderer.
@@ -16,25 +17,28 @@ public class Isoline : MonoBehaviour
     [Min(0.01f)] public float outerRadius = 2f;
     [Min(3)] public int segments = 32;
 
-    [Header("Optiona;")]
+    [Header("Optional")]
     public float yPosition = 0f; // height of the ring
 
     private Mesh mesh;
-    stopOnCollision sphere;
+    stopOnCollision sphere; // parent sphere
 
-   // public stopOnCollision[] spheres;
+    public GameObject heightReferenceObject;
+	public float heightReferenceFloat = 0;
+	float heightCutOff;
+	public float errorAdjustment=0;
+
+    MeshFilter meshFilter;
+    public Transform cutSphere;
+
 
     void Awake()
     {
-        //spheres = FindObjectsOfType<stopOnCollision>();
-         sphere = GetComponentInParent<stopOnCollision>();
+         sphere = GetComponentInParent<stopOnCollision>();// initilize parent sphere
         InitializeMesh();
         GenerateRingMesh();
-       
-       
     }
 
- 
 
     void OnValidate()
     {
@@ -53,7 +57,8 @@ public class Isoline : MonoBehaviour
         {
             mesh = new Mesh();
             mesh.name = "IsolineRing";
-            GetComponent<MeshFilter>().mesh = mesh;
+            meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh = this.mesh;
         }
         else
         {
@@ -62,23 +67,27 @@ public class Isoline : MonoBehaviour
     }
 
     void Update(){
-
-       //  foreach (var sphere in spheres)
-        //{
-            if (sphere.is_collide)
-            {
-
-                transform.position = new Vector3((float)sphere.gameObject.transform.position.x, (float)sphere.gameObject.transform.position.y, (float)sphere.gameObject.transform.position.z);
-                //Vector3 hillCenter = new Vector3(x,0,z);
-                outerRadius = sphere.radius*1.5f;
-                innerRadius = sphere.radius / (float)1.0526;   
-                  
-                GenerateRingMesh();
-            }
-        }
-
     
+        if (sphere.is_collide)
+        {
 
+        transform.position = new Vector3((float)sphere.gameObject.transform.position.x, (float)sphere.gameObject.transform.position.y, (float)sphere.gameObject.transform.position.z);
+        //Vector3 hillCenter = new Vector3(x,0,z);
+        outerRadius = sphere.radius*1.5f;
+        innerRadius = sphere.radius * 1.45f;   
+
+               
+                 
+        GenerateRingMesh();
+        collideIsoline();
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+
+         Debug.Log("Trigger entered by: " + other.gameObject.name);
+
+    }
    
     public void GenerateRingMesh()
     {
@@ -95,13 +104,32 @@ public class Isoline : MonoBehaviour
             float x = Mathf.Cos(angle);
             float z = Mathf.Sin(angle);
 
+        
+
             // Outer vertex
             vertices.Add(new Vector3(x * outerRadius, yPosition, z * outerRadius));
             uv.Add(new Vector2((float)i / segments, 1f));
 
+            // Vector3  globalVerticeOuter = transform.TransformPoint( vertices[vertices.Count - 1]);
+            //Vector3  globalVerticeOuter = transform.TransformPoint( vertices[vertices.Count - 1]);
+
+            //  Vector3  globalVerticeOuter = transform.TransformPoint( vertices[vertices.Count - 1]);
+            // if(globalVerticeOuter.z < 1 || globalVerticeOuter.x < 1)
+            // {
+            //     vertices[vertices.Count - 1] = Vector3.zero;
+            // }
+
             // Inner vertex
             vertices.Add(new Vector3(x * innerRadius, yPosition, z * innerRadius));
             uv.Add(new Vector2((float)i / segments, 0f));
+
+            //  Vector3  globalVerticeInner = transform.TransformPoint( vertices[vertices.Count - 1]);
+            // if(globalVerticeInner.z < 1 || globalVerticeInner.x < 1)
+            // {
+            //     vertices[vertices.Count - 1] = Vector3.zero;
+            // }
+
+            
         }
 
         // Generate triangles (quad strips)
@@ -116,6 +144,7 @@ public class Isoline : MonoBehaviour
             triangles.Add(i + 2);
         }
 
+
         // Assign to mesh
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
@@ -126,7 +155,17 @@ public class Isoline : MonoBehaviour
         mesh.Optimize();
     }
 
-    // Optional: call at runtime to update size dynamically
+    void collideIsoline(){
+        //myMesh.RecalculateNormals();
+        //myMesh.RecalculateBounds(); // Important for the collider to work correctly
+
+        meshFilter.mesh = this.mesh;      
+        MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = this.mesh;
+    }
+
+
+
     //public void UpdateRing(float newInner, float newOuter, int newSegments)
    // {
     //    innerRadius = Mathf.Clamp(newInner, 0f, newOuter);
